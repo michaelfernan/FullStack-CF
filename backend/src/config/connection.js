836070c2960b4b4af934c8d28b51e6475d9fcc69
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2024-08-03 22:22:56 -0300

    variaveis para deploy

--- backend/src/config/connection.js
+++ backend/src/config/connection.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const config = require('./config.json');
 
 const environment = process.env.NODE_ENV || 'development';
-const { username, password, database, host, dialect } = config[environment];
+const { username, password, database, host, port, dialect } = config[environment];
 
 const sequelize = new Sequelize(database, username, password, {
   host,
+  port,
   dialect,
   logging: false, // Set to true to see SQL queries in the console
 });
